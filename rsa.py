e39cf13e547e1c2e1292d9048142e3e91581b3bb
Darcy Clark <drgclark@uwaterloo.ca>
2024-11-03 14:09:21 +0100

    Compute d; fixed message again

--- rsa.py
+++ rsa.py
@@ -1,4 +1,30 @@
-print('Hello World!')
+def eea(sigma, e):
+    r_prev = sigma
+    r_curr = e
+
+    s_prev = 1
+    s_curr = 0
+
+    t_prev = 0
+    t_curr = 1
+
+    q = 0
+    while r_curr != 0:
+        q = r_prev // r_curr
+
+        temp = r_curr
+        r_curr = r_prev - (q * r_curr)
+        r_prev = temp
+
+        temp = s_curr
+        s_curr = s_prev - (q * s_curr)
+        s_prev = temp
+
+        temp = t_curr
+        t_curr = t_prev - (q * t_curr)
+        t_prev = temp
+
+    return t_prev
 
 message = input('Enter message: ')
 m = 0
@@ -6,7 +32,7 @@ m = 0
 iteration = 0
 for char in message:
     print('ASCII for "' + char + '": ' + str(ord(char)))
-    m = 256**iteration - 1 + ord(char)
+    m = m + ((256**iteration) * ord(char))
     iteration = iteration + 1
 
 print('m=' + str(m))
@@ -17,7 +43,7 @@ p = 41
 q = 61
 
 print('Using p=' + str(p) + ' and q=' + str(q))
-print('n=' + str(p * q))
+print('n: ' + str(p * q))
 
 sigma_num = (p - 1)*(q - 1)
 
@@ -29,19 +55,23 @@ while b != 0:
     b = a % b
     a = temp
 
-print('GCD is ' + str(a))
+print('GCD: ' + str(a))
 
 sigma = sigma_num // a
 
-print('Sigma is ' + str(sigma))
+print('Sigma: ' + str(sigma))
 
 e = 7
 
 cipher = m**e % (p * q)
 
-print('Cipher is: ' + str(cipher))
+print('Cipher: ' + str(cipher))
 
-d = 103 # temporarily hardcoded until I get the EEA
+d = eea(sigma, e)
+if d < 0:
+    d = d + sigma
+
+print('Computed d: ' + str(d))
 
 retrieved = cipher**d % (p * q)
-print('Retrieved message is: ' + str(retrieved))
+print('Retrieved message: ' + str(retrieved))
